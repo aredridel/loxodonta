2ff4c6a2d449d9a040d854e9a98fe154cebd2fe2
Aria Stewart <aredridel@nbtsc.org>
2017-04-12 22:20:05 -0400

    Sublevel the database

--- db.js
+++ db.js
@@ -1,15 +1,18 @@
 const promisifyLevelGraph = require('./promisify-levelgraph');
 const levelup = require('levelup');
 const leveldown = require('leveldown');
+const sublevel = require('level-sublevel');
 
 const levelgraph = require('levelgraph');
 const path = require('path');
 
-const db = promisifyLevelGraph(levelgraph(levelup(path.resolve(__dirname, 'db'), { db: leveldown }, err => { 
-	if (err) {
-		console.warn(`Database error: ${err.stack || err}`);
-		process.exit(1);
-	}
-})));
+const base = sublevel(levelup(path.resolve(__dirname, 'db'), { db: leveldown }, err => {
+    if (err) {
+        console.warn(`Database error: ${err.stack || err}`);
+        process.exit(1);
+    }
+}));
+
+const db = promisifyLevelGraph(levelgraph(base.sublevel('graph')));
 
 module.exports = db;
