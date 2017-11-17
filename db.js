bcc42684bd6dba2a5df6ca435f31b0a4c8c9f218
Aria Stewart <aredridel@dinhe.net>
2017-11-16 23:13:57 -0500

    Simplify db module

--- db.js
+++ db.js
@@ -6,39 +6,9 @@ const sublevel = require('level-sublevel');
 const levelPromise = require('level-promise');
 const levelgraphjsonld = require('levelgraph-jsonld');
 
-module.exports = function({config}) {
-    const db = levelPromise(sublevel(level(path.resolve(__dirname, config.db || 'db'), err => {
-        if (err) {
-            console.warn(`Database error: ${err.stack || err}`);
-            process.exit(1);
-        }
-    })));
-
-    const accounts = levelPromise(db.sublevel('accounts', { valueEncoding: 'json' }))
-    const posts = levelPromise(db.sublevel('posts', { valueEncoding: 'json' }));
-    const postsByAuthorBase = levelPromise(db.sublevel('postsByAuthor', { valueEncoding: 'json' }));
-    const postsByAuthor = {
-        forUser(user) {
-            return levelPromise(postsByAuthorBase.sublevel(user, { valueEncoding: 'json' }));
-        }
-    };
-    const timelinesBase = levelPromise(db.sublevel('timelines', { valueEncoding: 'json' }));
-    const timelines = {
-        forUser(user) {
-            return levelPromise(timelinesBase.sublevel(user, { valueEncoding: 'json' }))
-        }
-    };
-
-    const pubsubhubbubsubsBase = db.sublevel('pubsubhubbubhsubs', {valueEncoding: 'json'});
-
-    const pubsubhubbubsubs = {
-        forTopic(topic) {
-            return levelPromise(pubsubhubbubsubsBase.sublevel(topic, {valueEncoding: 'json'}));
-        }
-    };
-
+module.exports = function() {
     return {
-        graph: promisifyLevelGraph(levelgraphjsonld(levelgraph(db.sublevel('graph')))),
+        graph,
         db,
         posts,
         postsByAuthorBase,
@@ -49,6 +19,35 @@ module.exports = function({config}) {
         pubsubhubbubsubs,
         accounts
     }
-
-    process.on('SIGNUSR2', () => db.close())
 }
+const db = levelPromise(sublevel(level(path.resolve(__dirname, 'db'), err => {
+    if (err) {
+        console.warn(`Database error: ${err.stack || err}`);
+        process.exit(1);
+    }
+})));
+
+const accounts = levelPromise(db.sublevel('accounts', { valueEncoding: 'json' }))
+const posts = levelPromise(db.sublevel('posts', { valueEncoding: 'json' }));
+const postsByAuthorBase = levelPromise(db.sublevel('postsByAuthor', { valueEncoding: 'json' }));
+const postsByAuthor = {
+    forUser(user) {
+        return levelPromise(postsByAuthorBase.sublevel(user, { valueEncoding: 'json' }));
+    }
+};
+const timelinesBase = levelPromise(db.sublevel('timelines', { valueEncoding: 'json' }));
+const timelines = {
+    forUser(user) {
+        return levelPromise(timelinesBase.sublevel(user, { valueEncoding: 'json' }))
+    }
+};
+
+const pubsubhubbubsubsBase = db.sublevel('pubsubhubbubhsubs', {valueEncoding: 'json'});
+
+const pubsubhubbubsubs = {
+    forTopic(topic) {
+        return levelPromise(pubsubhubbubsubsBase.sublevel(topic, {valueEncoding: 'json'}));
+    }
+};
+
+const graph = promisifyLevelGraph(levelgraphjsonld(levelgraph(db.sublevel('graph'))));
