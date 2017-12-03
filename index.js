93bf7d61a3d426f28b775d905d7dee33ba9c456a
Aria Stewart <aredridel@dinhe.net>
2017-12-03 00:26:53 -0500

    Format

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 require('dotenv').config()
 const burpl = require('burpl');
-const { promisify } = require('util')
+const {promisify} = require('util')
 const listen = promisify(require('unix-listen'))
 
 const config = require('./config');
-const db = require('./db')({config});
+const db = require('./db')({
+  config
+});
 const App = require('./app');
 
 const match = require('fs-router')(__dirname + '/routes')
-const { send } = require('micro')
+const {send} = require('micro')
 
 module.exports = async function(req, res) {
   let matched = match(req)
   if (matched) return await matched(req, res)
-  send(res, 404, { error: 'Not found' })
+  send(res, 404, {
+    error: 'Not found'
+  })
 }
 
 const repl = burpl({
@@ -24,8 +28,17 @@ listen(repl, process.env.ADMINSOCKET || 'admin.sock')
 
 return
 
-const app = new App({ server, config, db });
-const ctx = { server, config, db, app };
+const app = new App({
+  server,
+  config,
+  db
+});
+const ctx = {
+  server,
+  config,
+  db,
+  app
+};
 
 require('./handlers/atom')(ctx);
 require('./handlers/salmon')(ctx);
