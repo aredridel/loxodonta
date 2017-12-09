846f7ca673c20d7e328448a10e48a28de3048cb2
Aria Stewart <aredridel@dinhe.net>
2017-12-09 16:41:52 -0500

    Extract addUser function

--- index.js
+++ index.js
@@ -33,33 +33,7 @@ const repl = burpl({
       object: 'https://github.com/aredridel/loxodonta/Account'
     }])).map(e => e.account).join('\n')
   },
-  "user NAME host HOST password PASSWORD": async (user, host, password) => {
-    const base = `https://${host}/@${user}/`
-
-    const db = await dbP
-
-    await db.jsonld.put({
-      "@context": "https://www.w3.org/ns/activitystreams",
-      "type": "Person",
-      "id": base,
-      "preferredUsername": user,
-      "inbox": url.resolve(base, 'inbox/'),
-      "outbox": url.resolve(base, 'outbox/'),
-      "followers": url.resolve(base, 'followers/'),
-      "following": url.resolve(base, 'following/'),
-      "liked": url.resolve(base, 'liked/')
-    })
-
-
-    await db.jsonld.put({
-      "@id": `acct:${user}@${host}`,
-      "@type": "https://github.com/aredridel/loxodonta/Account",
-      "https://github.com/aredridel/loxodonta/passwordScrypt": await scrypt.kdf(password, await scrypt.params(0.2)),
-      "https://github.com/aredridel/loxodonta/url": base
-    })
-
-    return `Added @${user}@${host} and setting password`
-  }
+  "user NAME host HOST password PASSWORD": addUser
 })
 
 listen(repl, process.env.ADMINSOCKET || 'admin.sock')
