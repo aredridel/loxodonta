5d7cbab03872a5511f3b007001b35322257928f6
Aria Stewart <aredridel@dinhe.net>
2017-12-09 01:14:20 -0500

    Start building a user/account separation

--- index.js
+++ index.js
@@ -25,7 +25,15 @@ module.exports = async function(req, res) {
 
 const repl = burpl({
   "die": () => process.exit(0),
-  "user add NAME host HOST password PASSWORD": async (user, host, password) => {
+  "show users": async () => {
+    const db = await dbP
+    return (await db.search([{
+      subject: db.v('account'),
+      predicate: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
+      object: 'https://github.com/aredridel/loxodonta/Account'
+    }])).map(e => e.account).join('\n')
+  },
+  "user NAME host HOST password PASSWORD": async (user, host, password) => {
     const base = `https://${host}/@${user}/`
 
     const db = await dbP
@@ -35,13 +43,19 @@ const repl = burpl({
       "type": "Person",
       "id": base,
       "preferredUsername": user,
-      "https://github.com/aredridel/loxodonta/username": user,
-      "https://github.com/aredridel/loxodonta/passwordScrypt": await scrypt.kdf(password, await scrypt.params(0.2)),
       "inbox": url.resolve(base, 'inbox/'),
       "outbox": url.resolve(base, 'outbox/'),
       "followers": url.resolve(base, 'followers/'),
       "following": url.resolve(base, 'following/'),
       "liked": url.resolve(base, 'liked/')
+    })
+
+
+    await db.jsonld.put({
+      "@id": `acct:${user}@${host}`,
+      "@type": "https://github.com/aredridel/loxodonta/Account",
+      "https://github.com/aredridel/loxodonta/passwordScrypt": await scrypt.kdf(password, await scrypt.params(0.2)),
+      "https://github.com/aredridel/loxodonta/url": base
     })
 
     return `Added @${user}@${host} and setting password`
--- routes/.well-known/webfinger.js
+++ routes/.well-known/webfinger.js
@@ -12,22 +12,30 @@ module.exports = async (req, res) => {
         if (!q.resource) throw new Error("Bad input: supply resource")
         const resource = url.parse(q.resource);
 
-        const user = await db.jsonld.get(q.resource, {})
-        console.warn(user)
+        const user = await db.jsonld.get(q.resource, {'@context': "https://www.w3.org/ns/activitystreams" })
+        console.warn(q.resource, user)
 
         if (!user) {
             throw Object.assign(new Error("user not found"), {statusCode: 404})
         }
 
+        const person = await db.jsonld.get(user['https://github.com/aredridel/loxodonta/url'], {'@context': "https://www.w3.org/ns/activitystreams" })
+
+        if (!person) {
+            throw Object.assign(new Error("account has no associated actor"), {statusCode: 500})
+        }
+
+        console.warn(person)
+
         res.setHeader('Content-Type', 'application/xrd+xml; charset=UTF-8');
 
         return xrdSchema.generate({
-            subject: `acct:${user.username}@${resource.host}`,
-            alias: `https://${resource.host}/@${user.username}`,
+            subject: user.id,
+            alias: person.id,
             links: [
-                { rel: "http://webfinger.net/rel/profile-page", type: "text/html", href: `https://${resource.host}/@${user.username}` },
-                { rel: "http://schemas.google.com/g/2010#updates-from", type: "application/atom+xml", href: `https://${resource.host}/users/${user.username}.atom` },
-                { rel: "salmon", href: `https://${resource.host}/api/salmon/${user.username}` },
+                { rel: "http://webfinger.net/rel/profile-page", type: "text/html", href: `https://${resource.host}/@${person.preferredUsername}` },
+                { rel: "http://schemas.google.com/g/2010#updates-from", type: "application/atom+xml", href: `https://${resource.host}/users/${person.preferredUsername}.atom` },
+                { rel: "salmon", href: `https://${resource.host}/api/salmon/${person.preferredUsername}` },
                 { rel: "magic-public-key", href: `data:application/magic-public-key,${user.public_key}` },
                 { rel: "http://ostatus.org/schema/1.0/subscribe", template: `https://${resource.host}/authorize_follow?acct={uri}` }
             ]
