ea7e2c4442b3f06cc365743818faf3d046fa0ecf
Aria Stewart <aredridel@nbtsc.org>
2017-12-27 21:18:42 -0800

    posts with URLs maybe?

--- post.js
+++ post.js
@@ -1,4 +1,7 @@
 const dbP = require('./db')()
+const context = require('./context')
+const parseAcct = require('./parseAcct')
+const typedId = require('./typedId')
 
 module.exports = async function (acct, content, summary) {
     const db = await dbP
@@ -6,7 +9,20 @@ module.exports = async function (acct, content, summary) {
     const post = {
         "@context": "https://www.w3.org/ns/activitystreams",
         "type": "Note",
+        id: await urlFor(acct, 'post'),
         summary,
         content,
     }
+
+    await db.jsonld.put(post, context)
+
+    return post.id
+}
+
+function urlFor(acct, type) {
+    if (type != 'post') throw new Error(`Unsupported type "${type}"`)
+
+    const { user, host } = parseAcct(acct)
+
+    return `//${host}/@${user}/${typedId.generate('post')}`
 }
