2060ecaee43fba09e1e6864857340b1329b91c74
Aria Stewart <aredridel@nbtsc.org>
2018-01-06 22:19:56 -0500

    Extract object-from-db module

--- routes/:user/:object.js
+++ routes/:user/:object.js
@@ -1,15 +1,1 @@
-const dbP = require('../../db')()
-const context = require('../../context')
-const selfurl = require('selfurl')
-const { send } = require('micro')
-
-module.exports = async (req, res) => {
-    const db = await dbP
-
-    const obj = await db.jsonld.get(selfurl(req), context)
-    if (obj) {
-        return obj
-    } else {
-        send(res, 404, "Not Found")
-    }
-}
+return require('../../objectFromDb')
